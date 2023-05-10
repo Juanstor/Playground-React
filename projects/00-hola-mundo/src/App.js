7018e52cc3b3c1a65e7b28475b9324f197c7d773
Juanstor <alvarezjuansbs@gmail.com>
2023-05-10 13:01:20 -0500

    twitter card editing...

--- projects/00-hola-mundo/src/App.js
+++ projects/00-hola-mundo/src/App.js
@@ -1,7 +1,28 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
 export function App () {
     return (
-        <div>
-            <h1>Twitter Card</h1>
-        </div>
+        <>
+            <TwitterFollowCard 
+                userName="midudev" 
+                name="Soy midevu" 
+                isFollowing={true}
+            />
+            <TwitterFollowCard 
+                userName="dog" 
+                name="Guau Guau" 
+                isFollowing={false}
+            />
+            <TwitterFollowCard 
+                userName="deviantart/cat" 
+                name="Miau michi" 
+                isFollowing
+            />
+            <TwitterFollowCard 
+                userName="shakira" 
+                name="waka waka" 
+            />
+        </>
     )
 }
